248b54ca66d72663334d44cfeecf0940bddc0863
Larissa Aiko Kian <larissaaiko2004@gmail.com>
2026-05-06 14:09:03 -0300

    Fix: Agregar ACL

--- __manifest__.py
+++ __manifest__.py
@@ -8,6 +8,7 @@
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
+        'security/security_groups.xml',
         'views/estate_property_views.xml',
         'views/res_users_views.xml',
         'report/estate_property_reports.xml'
